81b551b1ab29bf8b57c233d90621b1e23a7b1800
Kat B <katreesebarnes@gmail.com>
2019-03-03 22:41:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,10 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each_with_object({}) do |(key, hash), new_hash|
-    hash.each do |value, name|
-    
-    
-  # write your code here!
+    hash.each do |value, names|
+      binding.pry
+      
+      
+    end
   end
 end
