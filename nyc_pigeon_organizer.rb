2586c42d38d0db414e1f2efd142e43783ddd4717
Kat B <katreesebarnes@gmail.com>
2019-03-03 22:56:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each_with_object({}) do |(key, hash), new_hash|
     hash.each do |value, names|
-    
-      
-      
+      names.each do |name|
+        
+      end
     end
   end
 end
